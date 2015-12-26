ae8efd3f6e2dc4cc259f4209847ed073d36ac49b
Arjen <arjen.Swellengrebel@gmail.com>
2015-12-26 16:17:00 +0100

    kom and kdm for sage too

--- creator.py
+++ creator.py
@@ -38,33 +38,39 @@ class character:
 		print("damage 1d6+"+str(c.damage()))
 		print("fort "+str(c.save(FORT))+" ref "+str(c.save(REF))+" will "+str(c.save(WILL)))
 
-	def setclass(s,clas,savechoice=-1,roguechoice=-1): 
-		if (clas == MONK or clas == SAGE or clas == ROGU) and savechoice not in [FORT, REF, WILL]:
+	def setclass(s,clas,choiceone=-1,choicetwo=-1):
+		#choiceone is for rogue's save. choicetwo is for rogue's bab or skills.
+		#choiceone is for sage's kom. choicetwo is for sage's kdm. 
+		if (clas == MONK or clas == SAGE or clas == ROGU) and choiceone not in [FORT, REF, WILL]:
 			raise Exception("bad arguments")
-		if clas == ROGU and (savechoice == REF or roguechoice not in [SKILROGU, BABROGU]): 
+		if clas == ROGU and (choiceone == REF or choicetwo not in [SKILROGU, BABROGU]): 
+			raise Exception("bad arguments")
+		if clas == SAGE and (choiceone not in [INT,WIS,CHA] or choicetwo not in [STR,CON,DEX]): 
 			raise Exception("bad arguments")
 		
 		if clas == BARB or clas == PALA or clas == RANG: s.classhp = 10
 		else: s.classhp = 8
 		
 		if clas == SAGE or clas == SHAM or clas == TACT: s.goodbab = False
-		elif clas == ROGU and roguechoice == SKILROGU: s.goodbab = False
+		elif clas == ROGU and choicetwo == SKILROGU: s.goodbab = False
 		else: s.goodbab = True
 		
 		if clas == RANG: s.badsave = WILL
 		elif clas == TACT: s.badsave = FORT
-		elif clas == MONK or clas == ROGU or clas == SAGE: s.badsave = savechoice
+		elif clas == MONK or clas == ROGU or clas == SAGE: s.badsave = choiceone
 		else: s.badsave = REF
 		
 		if clas == MONK or clas == SHAM: s.kom = WIS
-		elif clas == RANG: s.kom == DEX
-		elif clas == TACT: s.kom == INT
-		# Sage's choice, Rogue's choice
+		elif clas == RANG: s.kom = DEX
+		elif clas == TACT: s.kom = INT
+		elif clas == SAGE: s.kom = choiceone
+		# Rogue's choice
 		else: s.kom = STR
 		
 		if clas == PALA or clas == SHAM: s.kdm = CHA
-		elif clas == RANG: s.kdm == INT
-		# Sage's choice, Rogue's choice
+		elif clas == RANG: s.kdm = INT
+		elif clas == SAGE: s.kdm = choicetwo
+		# Rogue's choice
 		else: s.kdm = CON
 
 c = character()
@@ -74,4 +80,3 @@ c.show()
 
 c.setclass(ROGU,WILL,BABROGU)
 c.show()
-
