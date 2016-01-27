25690a7906ccf605ee19dc51b46a105787fe995c
Jonathan Vorst - Concorde Group BV <Jonathan.Vorst@Concorde.nl>
2016-01-27 14:30:37 +0100

    Undo

--- dmstats.py
+++ dmstats.py
@@ -3,7 +3,8 @@ import random
 
 c = creator.character()
 c.setlevel(10)
-c.stats = [sum(sorted([random.randint(1,6) for _ in range(5)])[-3:]) for _ in range(6)]
+#c.stats = [sum(sorted([random.randint(1,6) for _ in range(5)])[-3:]) for _ in range(6)]
+c.stats = [14,10,16,10,14,12]
 c.setclass(creator.VAMP)
 c.setrace(creator.VAMP,creator.DEX)
 c.setskills([creator.ATHL,creator.PERC,creator.INTI,creator.ACRO,creator.LARC])
